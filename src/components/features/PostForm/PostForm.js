912eb5b6039653d1ba0d52b5d47a4ce34b735dea
Adrian Manczak <manczakadrian@gmail.com>
2023-12-01 23:22:12 +0100

    failure

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -12,6 +12,7 @@ import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
 
+
 const PostForm = ({ action, actionText, ... props}) => {
 
     const [title, setTitle] = useState(props.title || '');
